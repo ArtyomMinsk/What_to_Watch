485bfaf2324ef495be2aad26833ba48f28270a82
ArtyomMinsk <artem.nesterenko@gmail.com>
2016-07-24 12:16:26 -0400

    Added function that returns movie name

--- movie.py
+++ movie.py
@@ -1,60 +1,73 @@
 import csv
+#from rating import Rating
+
+
 
 class Movie:
 
-    #def __init(self, movieRow)
-    def __init__(self, movie_id, movie_title, release_date, video_release_date, IMDb_url,
-                    unknown, Action, Adventure, Animation, Childrens, Comedy, Crime, Documentary,
-                    Drama, Fantasy, Film_Noir, Horror, Musical, Mystery, Romance, Sci_Fi,
-                    Thriller, War, Western):
-        self.movie_id = movie_id
-        # self.movie_id = movieRow[0]
-        self.movie_title = movie_title
-        # self.movie_title = movieRow[1]
-        self.release_date = release_date
-        # self.release_date = movieRow[2]
-        self.video_release_date = video_release_date
-        self.IMDb_url = IMDb_url
-        self.unknown = unknown
-        self.Action = Action
-        self.Adventure = Adventure
-        self.Animation = Animation
-        self.Childrens = Childrens
-        self.Comedy = Comedy
-        self.Crime = Crime
-        self.Documentary = Documentary
-        self.Drama = Drama
-        self.Fantasy = Fantasy
-        self.Film_Noir = Film_Noir
-        self.Horror = Horror
-        self.Musical = Musical
-        self.Mystery = Mystery
-        self.Romance = Romance
-        self.Sci_Fi = Sci_Fi
-        self.Thriller = Thriller
-        self.War = War
-        self.Western = Western
+    #m = Movie()
 
-    def get_movie_id(self):
-        return self.movie_id
+    def __init__(self, row):
+        self.movie_id = row[0]
+        self.movie_title = row[1]
+        self.release_date = row[2]
+        self.video_release_date = row[3]
+        self.IMDb_url = row[4]
+        self.unknown = row[5]
+        self.Action = row[6]
+        self.Adventure = row[7]
+        self.Animation = row[8]
+        self.Childrens = row[9]
+        self.Comedy = row[10]
+        self.Crime = row[11]
+        self.Documentary = row[12]
+        self.Drama = row[13]
+        self.Fantasy = row[14]
+        self.Film_Noir = row[15]
+        self.Horror = row[16]
+        self.Musical = row[17]
+        self.Mystery = row[18]
+        self.Romance = row[19]
+        self.Sci_Fi = row[20]
+        self.Thriller = row[21]
+        self.War = row[22]
+        self.Western = row[23]
 
-    def get_title(self):
-        return self.movie_title
+        # Returns list of lists
+    def read_file():
+        #list of the lists
+        movie_rows_list = []
 
-    def get_release_date(self):
-        return self.release_date
+        with open('ml-100k/u.item', encoding = 'latin_1') as movie_file:
+            reader = csv.reader(movie_file, delimiter = '|')
+            for row in reader:
+                movie_rows_list.append(row)
+            print(movie_rows_list)
+            print("\ntitle: ", row[1])
+            print("\nmovie_rows_list[0]: ", movie_rows_list[0])
 
+        return movie_rows_list
 
-rows = []
+    # Return mobie names by movie_id as dictionary
+    def get_movie_name():
+        movie_rows_list = Movie.read_file()
+        for movie in movie_rows_list:
+            my_dict = dict([(int(movie[0]), movie[1])])
+            print(my_dict)
+        #movie_name = self.movie_title
 
-# def user_file():
-with open('ml-100k/u.item', encoding = 'latin_1') as movie_file:
-    reader = csv.reader(movie_file, delimiter = '|')
-    for row in reader:
-        rows.append(row)
-    print(rows)
-    print("\nrows[0]: ", rows[0])
+        return my_dict
 
+Movie.get_movie_name()
+
+    # def get_movie_id(self):
+    #     return self.movie_id
+    #
+    # def get_title(self):
+    #     return self.movie_title
+    #
+    # def get_release_date(self):
+    #     return self.release_date
 
 
     #####
